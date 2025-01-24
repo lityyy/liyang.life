45339f2ccf0bbf1593edf7d8be1e2b08b89cfe5f
liyang <49070556+lityyy@users.noreply.github.com>
2025-01-24 16:53:54 +0800

    Update edit-on-github.tsx

--- components/blog/edit-on-github.tsx
+++ components/blog/edit-on-github.tsx
@@ -5,7 +5,7 @@ export function EditOnGithub({ filePath }: { filePath: string }) {
   return (
     <Link
       className="text-gray-500 hover:text-gray-900 dark:hover:text-gray-100"
-      href={`${SITE_METADATA.siteRepo}/blob/main/data/${filePath}?plain=1`}
+      href={`${SITE_METADATA.siteRepo}/blob/master/data/${filePath}?plain=1`}
     >
       <GrowingUnderline data-umami-event="view-on-github">
         Edit on <span className="font-semibold">GitHub</span>
